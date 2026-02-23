fc5aa3c01dec1866a6d34d209743105994942074
Samuel Leiton N <samleighton02@gmail.com>
2026-02-23 15:29:08 -0600

    1.2

--- tmp/verify-token.mjs
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-import { jwtVerify } from 'jose';
-
-dotenv.config();
-const token = process.argv[2];
-const key = new TextEncoder().encode(process.env.AUTH_SECRET || '');
-
-const { payload } = await jwtVerify(token, key, { algorithms: ['HS256'] });
-console.log(JSON.stringify(payload, null, 2));
