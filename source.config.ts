95d4d2a96b132af126447578b5027c65f535d992
Samuel Leiton N <samleighton02@gmail.com>
2026-03-03 23:19:59 -0600

    1.4.8.2

--- source.config.ts
+++ source.config.ts
@@ -2,6 +2,9 @@ import { defineDocs, defineConfig } from 'fumadocs-mdx/config';
 
 export const docs = defineDocs({
   dir: 'docs',
+  meta: {
+    files: ['**/meta.json'],
+  },
 });
 
 export default defineConfig();
